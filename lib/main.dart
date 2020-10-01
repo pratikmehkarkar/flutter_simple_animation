323b906ae203c933b4448070d49989f495d157bd
Pratik Mehkarkar <pratikmehkarkar@gmail.com>
2020-10-01 15:15:51 -0400

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+//Code written by Pratik Mehkarkar to work on basic concepts of layout in flutter
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget
+{
+  //The root of flutter application
+  @override
+  Widget build(BuildContext context)
+  {
+    Color color = Theme.of(context).primaryColor;
+    return MaterialApp();
+  }
+}
+
+
