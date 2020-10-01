debf52e5f63ce3a814962475021486cb400ae449
Pratik Mehkarkar <pratikmehkarkar@gmail.com>
2020-10-01 15:18:42 -0400

    Second Commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,81 @@
-//Code written by Pratik Mehkarkar to work on basic concepts of layout in flutter
+//Flutter application written by pratik mehkarkar
 import 'package:flutter/material.dart';
+import 'package:flutter/animation.dart';
 
-void main() {
-  runApp(App());
+void main() => runApp(App());
+
+class App extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Animation',
+      home: FirstPage(),
+    );
+  }
 }
 
-class App extends StatelessWidget
+class FirstPage extends StatefulWidget
 {
-  //The root of flutter application
   @override
   Widget build(BuildContext context)
   {
-    Color color = Theme.of(context).primaryColor;
-    return MaterialApp();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Animation'),),
+    );
+  }
+
+  @override
+  FirstPageState createState() => FirstPageState();
+}
+
+class FirstPageState extends State<FirstPage>  with SingleTickerProviderStateMixin
+{
+  AnimationController controller;
+  Animation animation;
+  Animation size_animation;
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Homework 4'),
+      ),
+      body: Row(
+        children: [
+          Container(
+            height: size_animation.value,
+            width: size_animation.value,
+            color: animation.value,
+            child: FlutterLogo(),
+          ),
+          Container(
+            child: Column(
+              children: [
+                Icon(Icons.check_circle_outline,
+                  size: 150,
+                  color: Colors.blueAccent,)
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+  @override
+  void initState()
+  {
+    super.initState();
+    controller = AnimationController(vsync: this,duration: Duration(seconds: 2));
+    animation = ColorTween(begin: Colors.black12,end: Colors.yellow).
+    animate(CurvedAnimation(parent: controller, curve: Curves.easeIn));
+    size_animation = Tween<double>(begin: 100.0,end: 200.0).animate(controller);
+    controller.addListener(()
+    {
+      setState(() {});
+    });
+    controller.repeat();
   }
 }
 
